5ecc95bc3fcde38887e09a746b4ad4e254d35e8f
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-09-29 13:49:26 -0400

    notepad pt1

--- notepad.py
+++ notepad.py
@@ -11,3 +11,28 @@ print(comapracao_igual)
 # Comp Diferente
 comparacao_diferente = 1 != 1
 print(comapracao_igual)
+
+print("comparações")
+
+print(1 == "1") # False, pois os tipos são diferentes
+
+print("Atividade")
+
+print("dani" in "daniel")   # True
+print(50 > 25)  # True
+print(22 != 22) # False
+print("p" == "p") # True
+print(-2 == -3)
+
+# Operadores de comparação
+
+print("and, or e not")
+
+# and: Todos os blocos precisam ser True
+# not: Negação
+# or : Isso ou Aquilo. Verdadeiro de um dos lados já satisfaz
+print(True and True)
+print(True and False)
+print(False and False)
+print(not (False and True))
+print(not False and True)
