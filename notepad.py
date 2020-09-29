844fe7c9ab712feb6b0417e11b8eac8a31359610
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-09-29 13:30:11 -0400

    comparações

---
+++ notepad.py
@@ -0,0 +1,13 @@
+# Boolean -> Verdadeiro (True) ou Falso (False)
+administrador = True
+print(administrador)
+
+# Comparações
+
+# Comp Igual
+comapracao_igual = 1 == 1
+print(comapracao_igual)
+
+# Comp Diferente
+comparacao_diferente = 1 != 1
+print(comapracao_igual)
